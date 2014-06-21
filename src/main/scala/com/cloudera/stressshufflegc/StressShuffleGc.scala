9f5845f76f495c69bfa04caeba138b4b5e5227cd
Sandy Ryza <sandy@cloudera.com>
2014-06-20 23:39:28 -0700

    Code

---
+++ src/main/scala/com/cloudera/stressshufflegc/StressShuffleGc.scala
@@ -0,0 +1,55 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.cloudera.stressshufflegc
+
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+
+import scala.util.Random
+
+case class SomeObject(val name: String, num1: Int, num2: Int)
+
+object StressShuffleGc {
+  def main(args: Array[String]) {
+    val sc = new SparkContext(new SparkConf().setAppName("Spark Count"))
+
+    val numMappers = args(0).toInt
+    val numUniqueKeys = args(1).toInt
+    val recordsPerMapper = args(2).toInt / numMappers
+    val numReducers = args(3).toInt
+
+    val seeds = (0 until numMappers).toArray
+    val seedsRdd = sc.parallelize(seeds, numMappers)
+
+    val pairRdd = seedsRdd.flatMap(generateRecords(_, numUniqueKeys, recordsPerMapper))
+
+    pairRdd.groupByKey(numReducers).count()
+  }
+
+  def generateRecords(seed: Int, numUniqueKeys: Int, numRecords: Int):
+      TraversableOnce[(Integer, SomeObject)] = {
+    val rand = new Random(seed)
+    for (i <- 0 until numRecords) yield {
+      val randObject = SomeObject(rand.nextString(20), rand.nextInt(), rand.nextInt())
+      val randKey = rand.nextInt(numUniqueKeys)
+      (new Integer(randKey), randObject)
+    }
+  }
+}
